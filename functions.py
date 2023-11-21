004032684236b394c5861de55a95f3b9a0bfd13f
Ruubesh <ruubeshg27@gmail.com>
2023-11-21 21:54:14 +0100

    add methods for page1

--- functions.py
+++ functions.py
@@ -1,4 +1,39 @@
-def convert(entry_int, output_string):
-    mile_input = entry_int.get()
-    km_output = mile_input * 1.61
-    output_string.set(km_output)
+from tkinter import filedialog
+from cfg import CFG
+
+
+def open_file(file_variable):
+    filename = filedialog.askopenfilename(
+        initialdir="/Users/ruube/PycharmProjects/Thesis",
+        title="Select A File",
+        filetypes=(("Text files", "*.txt"), ("All files", "*.*"))
+    )
+
+    file_variable.set(filename)
+
+
+def submit(file, grammar_str):
+    with open(file, 'r') as f:
+        grammar = f.read()
+        grammar_str.set(grammar)
+
+
+def add(file, val_type, val, grammar_str):
+    config = CFG().read_config(file)
+    if val_type == 'rules':
+        CFG().add_rule_to_config(config, val)
+    else:
+        CFG().add_value(config, val_type, val)
+
+    submit(file, grammar_str)
+
+
+def remove(file, val_type, val, grammar_str):
+    config = CFG().read_config(file)
+    if val_type == 'rules':
+        CFG().remove_rule(config, val)
+    else:
+        CFG().remove_value(config, val_type, val)
+
+    submit(file, grammar_str)
+
